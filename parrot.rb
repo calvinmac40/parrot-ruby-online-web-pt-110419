0e4a7eb1216aa8061409a46c443b0bde387434be
Calvin McDowell <calvinmac40@hotmail.com>
2020-02-04 15:21:12 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
-def parrot(string = "Squawk!")
-  puts string
-  return string
-end
-=======
->>>>>>> 62ca91ce4ea7b036d430a31113b7a4366f6ae2e6
+# <<<<<<< HEAD
+# def parrot(string = "Squawk!")
+#   puts string
+#   return string
+# end
+# =======
+# >>>>>>> 62ca91ce4ea7b036d430a31113b7a4366f6ae2e6
